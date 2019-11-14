fddee989d213fe21db33bd3e417df210830d5353
mrchenliang <c.liang@ualberta.ca>
2019-11-14 10:45:45 -0500

    day 14 challenge

--- day14.js
+++ day14.js
@@ -1,3 +1,6 @@
+const numberOfPeople = 15;
+const distanceTraveled = 10;
+
 const dynamicPricing = (numberOfPeople, distanceTraveled) => {
   if (numberOfPeople >= 30) {
     return `$${(1 + distanceTraveled * 0.25 + 0.25).toFixed(2)}`;
