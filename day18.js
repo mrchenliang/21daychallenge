24a170d0ef8497078ad2b397e656805b88180fd8
mrchenliang <c.liang@ualberta.ca>
2019-11-18 08:56:45 -0500

    day 18 challenge

---
+++ day18.js
@@ -0,0 +1,26 @@
+const countTickets = tickets => {
+  let object = {
+    red: 0,
+    green: 0,
+    blue: 0
+  };
+  tickets.forEach(item => {
+    if (item in obj) {
+      object[item]++;
+    }
+  });
+  return obj;
+};
+
+const bestOdds = (tickets, raffleEntries) => {
+  let object = countTickets(tickets);
+  let odds = [];
+  for (let raffle in raffleEntries) {
+    odds.push([raffle, object[raffle] / raffleEntries[raffle]]);
+  }
+  odds.sort(function(a, b) {
+    return b[1] - a[1];
+  });
+
+  return `You have the best odds of winning the ${odds[0][0]} raffle.`;
+};
